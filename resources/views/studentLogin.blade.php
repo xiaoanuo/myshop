01b8878dd6d8130ba622c69f2b86dd240b4d67ba
unknown <xiaoanuo>
2019-07-11 18:58:40 +0800

    “fi““初始”

---
+++ resources/views/studentLogin.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+        <center>
+            <h1>登录</h1>
+                <form action="{{url('student/do_login')}}" method="post">
+                    @csrf
+                    <tr>
+                        <td>用户名</td>
+                        <td><input type="text" name="name"></td>
+                    </tr>
+                    <tr>
+                        <td>密码</td>
+                        <td><input type="text" name="password"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" name="" value="提交"></td>
+                    </tr>
+                </form>
+        </center>
+</body>
+</html>
